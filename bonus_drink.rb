cfef66f0419416c21bcd7de28eb6c1eee29ece5b
h-miyahara@it-hinataya.net <h-miyahara@it-hinataya.net>
2018-06-30 11:43:24 +0900

    First Try

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,17 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    drank = amount
+    loop {
+      bonus = amount / 3
+      remains = amount % 3
+      drank += bonus
+      if bonus == 0 then
+        break
+      end
+      amount = bonus + remains
+    }
+    drank
   end
 end
+
+p BonusDrink.total_count_for(100)
